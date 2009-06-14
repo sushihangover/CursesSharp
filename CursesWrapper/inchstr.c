f21870a2f4d3429094f8634ddda41198f4ba14c8
nythil <nythil@52a65036-936b-447d-8c72-270fba66c20f>
2009-06-14 09:25:47 +0000

    Added new files to makefile. Fixed warnings in Linux.
    git-svn-id: https://svn.code.sf.net/p/curses-sharp/code/trunk@49 52a65036-936b-447d-8c72-270fba66c20f

--- CursesWrapper/inchstr.c
+++ CursesWrapper/inchstr.c
@@ -68,11 +68,11 @@
 WRAP_API int
 wrap_winchnstr(WINDOW *win, unsigned int *ch, int n)
 {
-	return winchnstr(win, ch, n);
+	return winchnstr(win, (chtype*)ch, n);
 }
 
 WRAP_API int
 wrap_mvwinchnstr(WINDOW *win, int y, int x, unsigned int *ch, int n)
 {
-	return mvwinchnstr(win, y, x, ch, n);
+	return mvwinchnstr(win, y, x, (chtype*)ch, n);
 }
