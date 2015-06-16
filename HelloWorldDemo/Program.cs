8058b693e9a9e3b027fa68e3a506e3b544d796a0
SushiHangover <sushihangover@outlook.com>
2015-06-16 11:10:08 -0700

    Added HelloWorldDemo Based on: http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html Add to autotool config and to the VS/XS/MD CursesSharp.sln so you can build all the demos  via make, xbuild, mdtoot, or the IDE...

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using CursesSharp;
+
+namespace HelloWorldDemo
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// C style from http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
+//			initscr();			/* Start curses mode 		  */
+//			printw("Hello World !!!");	/* Print Hello World		  */
+//			refresh();			/* Print it on to the real screen */
+//			getch();			/* Wait for user input */
+//			endwin();			/* End curses mode		  */
+
+			Curses.InitScr ();
+			Stdscr.Add ("Hello World !!!");
+			Stdscr.Refresh ();
+			Stdscr.GetChar ();
+			Curses.Beep ();
+			Curses.EndWin();
+		}
+	}
+}
---
+++ HelloWorldDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("HelloWorldDemo")]
+[assembly: AssemblyDescription ("C# Curses Hello World Demo")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("http://sushihangover.github.io")]
+[assembly: AssemblyProduct ("CursesSharp")]
+[assembly: AssemblyCopyright ("2015 SushiHangover/RobertN")]
+[assembly: AssemblyTrademark ("All rights reserved : SushiHangover/RobertN")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
