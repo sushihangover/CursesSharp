368906549512446dc5e97f8c9214c482d6847aac
SushiHangover <sushihangover@outlook.com>
2015-07-20 11:10:57 -0700

    Add a native resize demo for a Stack Overflow question: http://stackoverflow.com/questions/31522500/mono-resize-terminal-on-mac-os-x/31523386#31523386

---
+++ CursesSharp.Demo/Demo.Native.ResizeTerm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Demo.Native.ResizeTerm
+{
+	class MainClass
+	{
+		[DllImport ("libc")]
+		private static extern int system (string exec);
+
+		[DllImport("ncurses")]
+		private static extern int resize_term(int nlines, int ncols);
+		[DllImport("ncurses")]
+		private static extern IntPtr initscr();
+
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Stack overflow, lets get bigger");
+			initscr ();
+			resize_term (50, 100);
+			system("resize -s 50 100 > /dev/null");
+			Console.WriteLine ("We will be 50 line and 100 columns in Terminal.app now");
+			Console.ReadKey ();
+		}
+	}
+}
